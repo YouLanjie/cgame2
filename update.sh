07c395c2f883ef33663c42a6d8a7a2479c5ddb0e
youlanjie <ylj18926385312@163.com>
2021-06-05 07:55:51 +0800

    增加英文版

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ clear
 if [ `dpkg --get-selections | grep cgame2 |wc -l` -eq 1 ]
 then
 	clear
-	sudo dpkg -r cgame2
+	sudo dpkg -r cgame2-en
 fi
-sudo dpkg -i ./build/cgame2.deb
-rm ./build/cgame2.deb
+sudo dpkg -i ./build/cgame2-en.deb
+rm ./build/cgame2-en.deb
 
